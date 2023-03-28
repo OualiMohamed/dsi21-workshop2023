d7395087d754a0c8e9bfb382926f50412d42ba38
Ouali Mohamed <mohamed.ouali2010gmail.com>
2023-03-28 18:08:21 +0100

    create student controller, model and migration

---
+++ database/migrations/2023_03_28_124157_create_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('ncin', 8);
+            $table->string('name', 100);
+            $table->string('email', 150);
+            $table->string('phone', 20);
+            $table->text('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
