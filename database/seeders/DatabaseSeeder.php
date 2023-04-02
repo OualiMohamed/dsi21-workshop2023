d55e8afc3321015aa5eb1493760e3050a05aed52
Ouali Mohamed <mohamed.ouali2010gmail.com>
2023-04-02 16:43:19 +0100

    create students seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use \App\Models\Student;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call([
+            StudentsSeeder::class
+        ]);
+
+        // Student::create([
+        //     'ncin' => '12234567',
+        //     'name' => 'Sahli Amani',
+        //     'email' => 'sahli@amani@gmail.com',
+        //     'phone' => '57667668',
+        //     'address' => 'Adresse de Amani',
+        // ]);
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
